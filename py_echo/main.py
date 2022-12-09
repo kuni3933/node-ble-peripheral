3038183a5dbed45ca30d17b7591f8d64537014d4
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2022-12-09 13:59:09 +0900

    [Update]fix: on時の関数を追加

--- py_echo/main.py
+++ py_echo/main.py
@@ -22,7 +22,7 @@ raspPiSerialNumber = getSerial()
 deviceName = "BerryLock_" + raspPiSerialNumber
 os.environ["BLENO_DEVICE_NAME"] = deviceName
 
-print('bleno - echo');
+print("bleno - echo");
 print("------------------------------")
 print("SerialNumber: " + raspPiSerialNumber)
 print("Initialize: " + deviceName)
@@ -30,7 +30,7 @@ print("------------------------------\n")
 
 
 def onStateChange(state):
-    print('on -> stateChange: ' + state);
+    print("on -> stateChange: " + state);
     if (state == 'poweredOn'):
         bleno.startAdvertising('echo', ['ec00'])
     else:
@@ -39,14 +39,29 @@ def onStateChange(state):
 bleno.on('stateChange', onStateChange)
 
 def onAdvertisingStart(error):
-    print('on -> advertisingStart: ' + ('error ' + error if error else 'success'));
+    print("on -> advertisingStart: " + ("error " + error if error else "success"));
 
     if not error:
         bleno.setServices([
             EchoService('ec00')
         ])
 
-bleno.on('advertisingStart', onAdvertisingStart)
+bleno.on("advertisingStart", onAdvertisingStart)
+
+
+def onAccept(clientAddress):
+    print("ble central connected: " + clientAddress)
+    bleno.updateRssi()
+
+bleno.on("accept", lambda clientAddress:onAccept(clientAddress))
+
+bleno.on("disconnect", lambda clientAddress: print("ble central disconnected: " + clientAddress));
+bleno.on("platform", lambda event: print("platform" + event));
+bleno.on("addressChange", lambda event: print("addressChange", event));
+bleno.on("mtuChange", lambda event: print("mtuChange", event));
+bleno.on("advertisingStartError", lambda event: print("advertisingStartError", event));
+bleno.on("servicesSetError", lambda event: print("servicesSetError", event));
+bleno.on("rssiUpdate", lambda event: print("rssiUpdate" + event));
 
 bleno.start()
 
